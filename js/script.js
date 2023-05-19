56c75de4c35d3b7874425dd85d66cd5afd20ed93
Chia-Chuan Tsou (Jason) <121275444+freshc0w@users.noreply.github.com>
2023-05-19 03:19:37 +0000

    Refactor code and add amore inline comments:

--- js/script.js
+++ js/script.js
@@ -11,13 +11,11 @@ window.onscroll = () => {
 	// When user scrolls, track current pageYOffset value.
 	let currPosScroll = window.pageYOffset;
 
-	// When user scrollls down, the curr scrolled position will be greater than
-	// the prev scrolled position. If this happens, hide navbar to the top.
-	// Otherwise, make it visible.
-	navBar.style.top = prevPosScroll < currPosScroll ? "-90px" : "0px";
-
-	// Style opacity of nav bar for a smoother disappearance effect.
-	navBar.style.opacity = prevPosScroll < currPosScroll ? "0" : "1";
+	// When user scrolls down, prevPosScroll will be < currPosScroll. If this
+	// condition is met, hide navbar to the top. Otherwise, make it visible.
+	// Opacity is also styled for a smoother disappearance effect.
+	[navBar.style.top, navBar.style.opacity] = // ES6 Destructuring assignment
+		prevPosScroll < currPosScroll ? ["-90px", "0"] : ["0px", "1"];
 
 	// Make sure to update previous scrolled position.
 	prevPosScroll = currPosScroll;
@@ -53,6 +51,7 @@ const allSkills = [
 	"angularjs",
 ];
 
+// Adds each skill in array to the skills map.
 updateSkills(allSkills, skills);
 
 /**
@@ -85,7 +84,8 @@ function updateSkills(skillsCollection, skillsMap) {
 			// e.g. if path to icon not found
 			console.error(`${name} icon not found. Please check name spelling. \n
             Error msg: ${err}.`);
-			// error squashed. iconDesc will be displayed in the skill container.
+			// error squashed. icon picture name will be displayed in the skill
+			// container.
 		}
 	};
 
@@ -136,6 +136,9 @@ function slideHorizontal(element, translateDist) {
 	element.style.transform = `translateX(${translateDist}px)`;
 }
 
+/**
+ * Shifts the carousel to the right by a certain width
+ */
 slideNext = () => {
 	// Move carousel train to the right by a container's width (115px).
 	slideHorizontal(skillsTrain, (currTrnsfrmedPos -= 115));
@@ -143,7 +146,8 @@ slideNext = () => {
 	// Appends more skill containers to the train after a set amount of clicks
 	// using modulo operator.
 	// e.g. if there are 30 total skills, a new set of skills will be appended
-	// after 20 "next" activations.
+	// after 20 "next" activations. This allows the last 10 to still appear
+    // in the train.
 	if (!((nextCounter % Object.keys(skills).length) / 1.5)) {
 		insertContainersAfter();
 		setContainersAddedCounter++;
@@ -168,12 +172,17 @@ slideNext = () => {
 };
 
 // Link click event with slideNext function for nextBtn.
-nextBtn.addEventListener("click", slideNext); 
+nextBtn.addEventListener("click", slideNext);
 
-// Add autoplay for carousel;
+// Add autoplay for carousel that activates next() every 2 seconds;
 // See https://www.w3schools.com/jsref/met_win_setinterval.asp
 setInterval(slideNext, 2000);
 
+
+/**
+ * Shifts the carousel to the left by a certain width. If carousel's position
+ * is at the start, do nothing. 
+ */
 slidePrev = () => {
 	slideHorizontal(
 		skillsTrain,
@@ -186,39 +195,40 @@ slidePrev = () => {
 prevBtn.addEventListener("click", slidePrev);
 
 /**
- * 
- * @param {String} iconName Skill / Technology image name 
+ * A skill container that contains all the relevant information of the skill.
+ * @param {String} iconName Skill / Technology image name
  * @param {String} iconDesc Brief description of the targeted image
- * @param {String} iconPath image src url path link. 
+ * @param {String} iconPath image src url path link.
  * @param {Number} num Tracker that identifies the skill number.
  * @returns {Element} A container div that houses all relevant information
  * regarding the skill.
  */
 function makeSkillContainer(iconName, iconDesc, iconPath, num) {
-	const container = document.createElement("div"); 
+	const container = document.createElement("div"); // Wrapper div
 
 	const skillIcon = document.createElement("img"); // Skill icon image
 	const skillName = document.createElement("h1"); // Skill name
 	const skillNum = document.createElement("span"); // Identifier
 
+    // Add all relevant classes for styling
 	container.classList.add("tool-container");
 	skillIcon.classList.add("skill-icon");
 	skillName.classList.add("skill-name");
 	skillNum.classList.add("skill-num");
 
-	skillIcon.setAttribute("src", iconPath);
-	skillIcon.setAttribute("alt", iconDesc);
-	skillName.textContent = iconName;
+	skillIcon.setAttribute("src", iconPath); // Link src path to retrieve img
+	skillIcon.setAttribute("alt", iconDesc); // Image name
+	skillName.textContent = iconName; 
 	skillNum.textContent = num;
 
-	[skillIcon, skillName, skillNum].forEach((elem) =>
-		container.appendChild(elem)
+	[skillIcon, skillName, skillNum].forEach((property) =>
+		container.appendChild(property)
 	);
 	return container;
 }
 
 /**
- * Append all skills to skillsTrain. 
+ * Append all skills to skillsTrain.
  * @param {Number} numTracker Identity number that keeps track of each skill.
  * e.g. first skill is 0, second skill is 1...
  */
@@ -234,26 +244,39 @@ function insertContainersAfter(numTracker = 0) {
 		);
 	}
 }
-
 /* Skills carousel end */
 
 /* Project event listeners*/
+
+// Project containers that houses each indivudal project.
 const projects = document.querySelectorAll(".project");
+
+// Modals inside the project containers that houses the project name and logo.
 const projectModals = document.querySelectorAll(".project__modal-container");
+
+// Overlay containers contains the relevant information, including a background
+// image of the project when the project container is clicked upon.
 const projectOverlays = document.querySelectorAll(".project-overlay");
+
+// Blur overlays darkens the project's background-image to make the foreground 
+// information standout.
 const projectBlurOverlays = document.querySelectorAll(".blur-overlay");
+
+// All close buttons inside the overlays that reinitialises the project states.
 const projectCloseBtns = document.querySelectorAll(".project__close--btn");
 
+// For each project container, link click event with expand function.
 projects.forEach((proj) => {
 	proj.addEventListener("click", expands);
 });
 
 function expands() {
+	// "this" is referring to the project container that is being clicked upon.
 	if (!this.classList.contains("project-active")) {
 		this.classList.add("project-active");
 	}
 
-	// Hides all other projects when animating.
+	// Hides all other projects (that is not active) when animating.
 	projects.forEach((proj) => {
 		if (!proj.classList.contains("project-active")) {
 			proj.style.visibility = "hidden";
@@ -272,24 +295,39 @@ function expands() {
 	});
 }
 
+// Link click event listeners with close buttons inside the project containers.
 projectCloseBtns.forEach((btn) => {
 	btn.addEventListener("click", (e) => {
 		projects.forEach((proj) => {
+			// Remove click event listener for project container so that user
+			// cannot expand any other projects.
 			proj.removeEventListener("click", expands);
+
+			// Remove active class that makes the overlay expands.
 			if (proj.classList.contains("project-active")) {
 				proj.classList.remove("project-active");
 			}
+
+			// Need to set some sort of delay to when adding click event listeners
+			// for each project containers so that the user doesn't accidentally
+			// expands new project when double clicking.
 			setTimeout(() => {
 				proj.addEventListener("click", expands);
 			}, 200);
 		});
+
+		// Change all project container back to visible.
 		projects.forEach((proj) => {
 			proj.style.visibility = "visible";
 		});
+
+		// Show modal displaying relevant information.
 		projectModals.forEach((modal) => {
 			modal.style.visibility = "visible";
 			modal.style.display = "flex";
 		});
+
+		// Blur background-image behind the modal information.
 		projectBlurOverlays.forEach((blurOverlay) => {
 			blurOverlay.style.backdropFilter = "blur(3.5px)";
 		});
@@ -309,14 +347,14 @@ submitFormBtn.addEventListener("click", (e) => {
 
 	const userName = document.getElementById("input-name")
 		? document.getElementById("input-name")
-		: "";
-	userName.value.style = "color: blue;";
-	alertText.textContent =
-		"I apologise " +
-		userName.value +
-		". Backend for info collection is" +
-		" not set up yet. Try contacting me in one of the other links.";
+		: "user";
 
+	userName.value.style = "color: blue;";
+	alertText.textContent = `I apologise ${userName.value}. 
+    Backend for info collection is not set up yet.
+    Try contacting me in of the other links.`;
+
+	// Make sure not to repeatedly add alert message.
 	if (!document.contains(document.querySelector(".contact__alert-text"))) {
 		contactForm.appendChild(alertText);
 	}
